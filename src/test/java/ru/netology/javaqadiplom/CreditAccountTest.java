4d14207d382e9d87149ad21d3e0da5b68a780293
pavel <gitkornienkov@gmail.com>
2023-04-12 22:57:34 +0300

    Add test 12.04.2023

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -9,12 +9,10 @@ public class CreditAccountTest {
 
     @ParameterizedTest
     @CsvSource({
-            "0,5000,5,-5000,5000", //баланс 0, кредитный лимит 5000, покупка 5000
-            "1000,5000,5,-4000,5000", //баланс 1000, кредитный лимит 5000, ожидаемый баланс после покупки -4000, стоимость покупки  5000
-            "5000,5000,5,0,5000", //баланс 5000, лимит 5000, покупка 5000, ожидаемый баланс 0
-            "10000,5000,5,5000,5000", //баланс 10000, лимит 5000, покупка 5000, ожидаемый баланс 5000
-            "0,3000,5,error,5000", //не вписывается в кредитный лимит, должно быть исключение
-            "2000,3000,5,4000,-1000" //покупка суммой бплпнса и кредита
+            "0,5000,5,-5000,5000", //оплата только кредитным лимитом, ожидаемый баланс -5000
+            "1000,5000,5,-4000,5000", //смешанная оплата, ожидаемый баланс -4000
+            "5000,5000,5,0,5000", //оплата за счет баланса, ожидаемый баланс - 0
+            "10000,5000,5,5000,5000", //оплата за счет баланса, ожидаемый баланс 5000
     })
     public void pay(int initialBalance, int creditLimit, int rate, int expected, int amount) {
         CreditAccount account = new CreditAccount(initialBalance, creditLimit, rate);
@@ -23,4 +21,34 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(expected, account.getBalance());
     }
+
+
+    @Test
+    public void negativeRateTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(5000,5000,-1);
+        });
+    }
+    @Test
+    public void zeroRateTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(5000,5000,0);
+        });
+    }
+
+    @Test
+    public void negativeInitialBalanceTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(-1,5000,5);
+        });
+    }
+
+    @Test
+    public void negativeCreditLiminTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(1000,-1,5);
+        });
+    }
+
+
 }
