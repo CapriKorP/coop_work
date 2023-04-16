72292c86adae5a34dccf8dacb8bc9aa34bb5deff
Anastasiia <anastasia994@yandex.ru>
2023-04-16 16:44:26 +0500

    Added 3 tests 16.04

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -1,4 +1,4 @@
-package ru.netology.javaqadiplom;
+/* package ru.netology.javaqadiplom;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,3 +19,4 @@ public class CreditAccountTest {
     }
 
 }
+*/
